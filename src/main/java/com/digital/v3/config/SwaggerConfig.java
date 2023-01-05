dae0f9dc8153a4dd60d066cf5342b1c01b1e3ebc
lee-code712 <yurilee07@naver.com>
2023-01-05 15:50:58 +0900

    swagger title 변경

--- src/main/java/com/digital/v3/config/SwaggerConfig.java
+++ src/main/java/com/digital/v3/config/SwaggerConfig.java
@@ -50,7 +50,7 @@ public class SwaggerConfig {
 	
 	private ApiInfo getApiInfo() {
         return new ApiInfoBuilder()
-        		.title("API-G")
+        		.title("API Gateway")
                 .description("Commerce API Gateway")
                 .build();
     }
